6f878ecb4730cbfbdb134f2ff464c311b8a0289c
yoavellinson <yoavellinson@gmail.com>
2023-05-30 11:45:09 +0300

    finished q3

--- code_ex_1/spectral_estimation.py
+++ code_ex_1/spectral_estimation.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import Rxx_hat_b
+from utils import *
 from tqdm import tqdm
 
 #Q1
@@ -19,8 +19,7 @@ sigma_w2 = np.sqrt(0.86)
 n1 =1024
 n2 = 2048
 
-
-def gen_signals(sigma_w1=np.sqrt(1/26),n1=1024,sigma_w2=np.sqrt(0.86),n2=2048):
+def gen_signals(sigma_w1=np.sqrt(1/26),n1=1024,sigma_w2=np.sqrt(0.86),n2=2048): #for the question
     w1 = np.random.normal(0,sigma_w1,n1)
     x1 = np.array([w1[n]-3*w1[n-1]-4*w1[n-2] for n in range(n1)])
 
@@ -35,69 +34,9 @@ def gen_signals(sigma_w1=np.sqrt(1/26),n1=1024,sigma_w2=np.sqrt(0.86),n2=2048):
     x2 = x2_tmp[-1024:]
     return x1,x2
 
-
-def calc_periodegram(x,M=4096):
-    x = np.pad(x,(0,M-x.shape[0]),'constant')
-    X = np.fft.fft(x,n=M) 
-    Sx_per = (1/len(x))*(np.abs(X)**2)[:int(M/2)+1]
-    w_M = np.linspace(0,2*np.pi,M)[:int(M/2)+1]
-    return Sx_per,w_M
-
-
-def calc_correlogram(x,M=4096):
-    x = np.pad(x,(0,int(M/2)+1-x.shape[0]),'constant')
-    N=len(x)
-    w_M = np.linspace(0,2*np.pi,M)[:int(M/2)+1]
-    Rx_hat = np.correlate(x,x,'full')/N
-    w = np.concatenate((-w_M[::-1],w_M[1:]))
-    w_mat = np.repeat(w.reshape(w.shape[0],1),w.shape[0],axis=1)
-    l = np.zeros(w_mat.shape)
-    l[0,:] = np.arange(-int(M/2),int(M/2)+1,1)
-    wl = np.dot(w_mat,l)
-    Sx_cor = np.dot(np.exp(-1j*wl),Rx_hat).real[-2049:]
-    return Sx_cor,w_M
-
-def calc_blackman_tukey(x,L=1,M=4096):
-    N=len(x)
-    x = np.pad(x,(0,int(M/2)+1-x.shape[0]),'constant')
-    w_M = np.linspace(0,2*np.pi,M)[:int(M/2)+1]
-    Rx_hat = np.correlate(x,x,'full')/N
-    l0 = np.argmax(Rx_hat)
-    win = np.zeros(Rx_hat.shape)
-    win[l0-L+1:l0+L] = np.ones((2*L-1))
-    Rx_hat_win = np.multiply(Rx_hat,win)
-    w = np.concatenate((-w_M[::-1],w_M[1:]))
-    w_mat = np.repeat(w.reshape(w.shape[0],1),w.shape[0],axis=1)
-    l = np.zeros(w_mat.shape)
-    l[0,:] = np.arange(-int(M/2),int(M/2)+1,1)
-    wl = np.dot(w_mat,l)
-    Sx_cor = np.dot(np.exp(-1j*wl),Rx_hat_win).real[-2049:]
-    return Sx_cor,w_M
-
-def calc_bartlet(x,k,M=4096):
-    N = len(x)
-    if(not N%k):
-        L = int(N/k)
-        x_frames = [np.pad(x[(i*L):(i*L)+L],(0,M-L),'constant') for i in range(k)]
-        Sx_k = np.array([(np.abs(np.fft.fft(x_frames[i]))**2)/L for i in range(k)])
-        Sx_b = np.array(1/k)*(np.sum(Sx_k,axis=0))[:int(M/2)+1]
-        w_M = np.linspace(0,2*np.pi,M)[:int(M/2)+1]
-        return Sx_b,w_M
-    else:
-        print('not a valid value for k')
-        return None,None
-
-def calc_welch(x,k,D,L,M=4096):
-    N = len(x)
-    x_frames = np.array([np.pad(x[i:i+L],(0,M-L),'constant') for i in np.multiply(list(range(k)),L-D)])
-    Sx_k = np.array([(np.abs(np.fft.fft(x_frames[i]))**2)/L for i in range(k)])
-    Sx_b = np.array((1/k)*(np.sum(Sx_k,axis=0)))[:int(M/2)+1]
-    w_M = np.linspace(0,2*np.pi,M)[:int(M/2)+1]
-    return Sx_b,w_M
-    
-
-Mc = 10
+Mc = 100
 Sx1_per = np.empty((Mc,2049))
+Sx1_cor = np.empty((Mc,2049))
 Sx1_bartlet_64 = np.empty((Mc,2049))
 Sx1_bartlet_16 = np.empty((Mc,2049))
 Sx1_welch_64 = np.empty((Mc,2049))
@@ -106,6 +45,7 @@ Sx1_bt_4 = np.empty((Mc,2049))
 Sx1_bt_2 = np.empty((Mc,2049))
 
 Sx2_per = np.empty((Mc,2049))
+Sx2_cor = np.empty((Mc,2049))
 Sx2_bartlet_64 = np.empty((Mc,2049))
 Sx2_bartlet_16 = np.empty((Mc,2049))
 Sx2_welch_64 = np.empty((Mc,2049))
@@ -113,17 +53,21 @@ Sx2_welch_16 = np.empty((Mc,2049))
 Sx2_bt_4 = np.empty((Mc,2049))
 Sx2_bt_2 = np.empty((Mc,2049))
 
+Sx1_analytic = Sxx1(w)
+Sx2_analytic = Sxx2(w)
+#getting all data:
 for i in tqdm(range(Mc)):
     x1,x2=gen_signals()
-    Sx1_per[i],_= calc_periodegram(x1) 
-    Sx1_bartlet_64[i],_ = calc_bartlet(x1,64) 
-    Sx1_bartlet_16[i],_ =calc_bartlet(x1,16) 
-    Sx1_welch_64[i],_ = calc_welch(x1,k=61,L=64,D=48) 
-    Sx1_welch_16[i],_ = calc_welch(x1,k=253,L=16,D=12) 
-    Sx1_bt_4[i],_ = calc_blackman_tukey(x1,L=4)
+    Sx1_per[i],_= calc_periodegram(x1)
+    Sx1_cor[i],_= calc_correlogram(x1)     
+    Sx1_bartlet_64[i],_ = calc_bartlet(x1,64)    
+    Sx1_bartlet_16[i],_ =calc_bartlet(x1,16)    
+    Sx1_welch_64[i],_ = calc_welch(x1,k=61,L=64,D=48)    
+    Sx1_welch_16[i],_ = calc_welch(x1,k=253,L=16,D=12)    
+    Sx1_bt_4[i],_ = calc_blackman_tukey(x1,L=4)    
     Sx1_bt_2[i],_ = calc_blackman_tukey(x1,L=2)
-
-    Sx2_per[i],_= calc_periodegram(x2) 
+    Sx2_per[i],_= calc_periodegram(x2)
+    Sx2_cor[i],_= calc_correlogram(x2)
     Sx2_bartlet_64[i],_ = calc_bartlet(x2,64) 
     Sx2_bartlet_16[i],_ = calc_bartlet(x2,16) 
     Sx2_welch_64[i],_ = calc_welch(x2,k=61,L=64,D=48)
@@ -131,52 +75,191 @@ for i in tqdm(range(Mc)):
     Sx2_bt_4[i],_ =  calc_blackman_tukey(x2,L=4)
     Sx2_bt_2[i],w_M = calc_blackman_tukey(x2,L=2)
 
-def get_avg(Sx,M=4096):
-    N = len(Sx)
-    Sx = np.array(Sx).reshape(N,int(M/2)+1)
-    Sx_mean = np.sum(Sx,axis=0)/N
-    return Sx_mean.flatten()
+#analysing stats
+#x1
+Sx1_per_bar,Sx1_per_bias,Sx1_per_var,Sx1_per_mse = get_all_stats(Sx1_per,Sx1_analytic)
+Sx1_cor_bar,Sx1_cor_bias,Sx1_cor_var,Sx1_cor_mse = get_all_stats(Sx1_cor,Sx1_analytic)
+Sx1_bartlet_64_bar,Sx1_bartlet_64_bias,Sx1_bartlet_64_var,Sx1_bartlet_64_mse = get_all_stats(Sx1_bartlet_64,Sx1_analytic)
+Sx1_bartlet_16_bar,Sx1_bartlet_16_bias,Sx1_bartlet_16_var,Sx1_bartlet_16_mse = get_all_stats(Sx1_bartlet_16,Sx1_analytic)
+Sx1_welch_64_bar,Sx1_welch_64_bias,Sx1_welch_64_var,Sx1_welch_64_mse = get_all_stats(Sx1_welch_64,Sx1_analytic)
+Sx1_welch_16_bar,Sx1_welch_16_bias,Sx1_welch_16_var,Sx1_welch_16_mse = get_all_stats(Sx1_welch_16,Sx1_analytic)
+Sx1_bt_4_bar,Sx1_bt_4_bias,Sx1_bt_4_var,Sx1_bt_4_mse = get_all_stats(Sx1_bt_4,Sx1_analytic)
+Sx1_bt_2_bar,Sx1_bt_2_bias,Sx1_bt_2_var,Sx1_bt_2_mse = get_all_stats(Sx1_bt_2,Sx1_analytic)
+#x2
+Sx2_per_bar,Sx2_per_bias,Sx2_per_var,Sx2_per_mse = get_all_stats(Sx2_per,Sx2_analytic)
+Sx2_cor_bar,Sx2_cor_bias,Sx2_cor_var,Sx2_cor_mse = get_all_stats(Sx2_cor,Sx2_analytic)
+Sx2_bartlet_64_bar,Sx2_bartlet_64_bias,Sx2_bartlet_64_var,Sx2_bartlet_64_mse = get_all_stats(Sx2_bartlet_64,Sx2_analytic)
+Sx2_bartlet_16_bar,Sx2_bartlet_16_bias,Sx2_bartlet_16_var,Sx2_bartlet_16_mse = get_all_stats(Sx2_bartlet_16,Sx2_analytic)
+Sx2_welch_64_bar,Sx2_welch_64_bias,Sx2_welch_64_var,Sx2_welch_64_mse = get_all_stats(Sx2_welch_64,Sx2_analytic)
+Sx2_welch_16_bar,Sx2_welch_16_bias,Sx2_welch_16_var,Sx2_welch_16_mse = get_all_stats(Sx2_welch_16,Sx2_analytic)
+Sx2_bt_4_bar,Sx2_bt_4_bias,Sx2_bt_4_var,Sx2_bt_4_mse = get_all_stats(Sx2_bt_4,Sx2_analytic)
+Sx2_bt_2_bar,Sx2_bt_2_bias,Sx2_bt_2_var,Sx2_bt_2_mse = get_all_stats(Sx2_bt_2,Sx2_analytic)
 
-Sx1_per_Mc = get_avg(Sx1_per)
-Sx1_bartlet_64_Mc = get_avg(Sx1_bartlet_64)
-Sx1_bartlet_16_Mc = get_avg(Sx1_bartlet_16)
-Sx1_welch_64_Mc = get_avg(Sx1_welch_64)
-Sx1_welch_16_Mc = get_avg(Sx1_welch_16)
-Sx1_bt_4_Mc = get_avg(Sx1_bt_4)
-Sx1_bt_2_Mc = get_avg(Sx1_bt_2)
 
-Sx2_per_Mc = get_avg(Sx2_per)
-Sx2_bartlet_64_Mc = get_avg(Sx2_bartlet_64)
-Sx2_bartlet_16_Mc = get_avg(Sx2_bartlet_16)
-Sx2_welch_64_Mc = get_avg(Sx2_welch_64)
-Sx2_welch_16_Mc = get_avg(Sx2_welch_16)
-Sx2_bt_4_Mc = get_avg(Sx2_bt_4)
-Sx2_bt_2_Mc = get_avg(Sx2_bt_2)
+#plots######
 
+#q2 plot todo
 fig,(ax1,ax2) = plt.subplots(nrows=1,ncols=2,figsize=(10,10))
-fig.suptitle(r'$\bar{S}_{x1}$ and $\bar{S}_{x2}$ Estimations for Mc'+f'={Mc}')
+fig.suptitle(r'$\bar{S}_{x1}$ and $\bar{S}_{x2}$ Estimations with the Correlogram and Periodegram methods')
 ax1.title.set_text(r'$\bar{S}_{x1}$')
 ax1.set_xlabel(r'$\omega$')
+ax1.set_ylabel(r'$\bar{S}_{x1}(e^{j\omega})$')
 ax2.title.set_text(r'$\bar{S}_{x2}$')
 ax2.set_xlabel(r'$\omega$')
-ax1.plot(w_M,Sx1_per_Mc,label = 'Sx1 Periodegram ($e^{jw} $)')
-ax1.plot(w_M,Sx1_bt_2_Mc,label = 'Sx1 BT L=2 ($e^{jw} $)')
-ax1.plot(w_M,Sx1_bt_4_Mc,label = 'Sx1 BT L=4 ($e^{jw} $)')
-ax1.plot(w_M,Sx1_bartlet_16_Mc,label = 'Sx1 Bartlet L=16 ($e^{jw} $)')
-ax1.plot(w_M,Sx1_bartlet_64_Mc,label = 'Sx1 Bartlet L=64 ($e^{jw} $)')
-ax1.plot(w_M,Sx1_welch_16_Mc,label = 'Sx1 Welch L=16 ($e^{jw} $)')
-ax1.plot(w_M,Sx1_welch_64_Mc,label = 'Sx1 Welch L=64 ($e^{jw} $)')
-ax2.plot(w_M,Sx2_per_Mc,label = 'Sx2 Periodegram ($e^{jw} $)')
-ax1.plot(w_M,Sxx1(w_M),linewidth=1.5,label = 'Sx1 True($e^{jw} $)')
-ax2.plot(w_M,Sx2_bt_2_Mc,label = 'Sx2 BT L=2 ($e^{jw} $)')
-ax2.plot(w_M,Sx2_bt_4_Mc,label = 'Sx2 BT L=4 ($e^{jw} $)')
-ax2.plot(w_M,Sx2_bartlet_16_Mc,label = 'Sx2 Bartlet L=16 ($e^{jw} $)')
-ax2.plot(w_M,Sx2_bartlet_64_Mc,label = 'Sx2 Bartlet L=64 ($e^{jw} $)')
-ax2.plot(w_M,Sx2_welch_16_Mc,label = 'Sx2 Welch L=16 ($e^{jw} $)')
-ax2.plot(w_M,Sx2_welch_64_Mc,label = 'Sx2 Welch L=64 ($e^{jw} $)')
-ax2.plot(w_M,Sxx2(w_M),linewidth=1.5,label = 'Sx2 True ($e^{jw} $)')
+ax2.set_ylabel(r'$\bar{S}_{x2}(e^{j\omega})$')
+ax1.plot(w_M,Sx1_per[0],label = 'Sx1 Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_cor[0],label = 'Sx1 Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sxx1(w_M),linewidth=1.5,label = 'Sx1 True($e^{j\omega} $)')
+
+ax2.plot(w_M,Sx2_per[0],label = 'Sx2 Periodegram ($e^{j\omega} $)')
+ax2.plot(w_M,Sx2_cor[0],label = 'Sx2 Correlogram ($e^{j\omega} $)')
+ax2.plot(w_M,Sxx2(w_M),linewidth=1.5,label = 'Sx2 True ($e^{j\omega} $)')
 ax1.legend(loc='upper right')
 ax2.legend(loc='upper right')
 plt.subplots_adjust(wspace=0.4,
                     hspace=0.4)
-plt.show()
+plt.savefig('pics/q2.png')
+
+#q3 plot
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$\bar{S}_{x1}$ Estimation for Mc'+f'={Mc}')
+ax1.set_xlabel(r'$\omega$')
+ax1.set_ylabel(r'$\bar{S}_{x1}(e^{j\omega})$')
+ax1.plot(w_M,Sx1_per_bar,label = r'$\bar{S}_{x1}$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_cor_bar,label = r'$\bar{S}_{x1}$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_2_bar,label = r'$\bar{S}_{x1}$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_4_bar,label = r'$\bar{S}_{x1}$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_16_bar,label = r'$\bar{S}_{x1}$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_64_bar,label = r'$\bar{S}_{x1}$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_16_bar,label = r'$\bar{S}_{x1}$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_64_bar,label = r'$\bar{S}_{x1}$ Welch L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sxx1(w_M),linewidth=1.5,label = r'${S}_{x1}$ True($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx1_bar.png')
+# plt.show()
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$B(e^{j\omega})$ for Mc'+f'={Mc}')
+ax1.set_xlabel(r'$\omega$')
+ax1.set_ylabel(r'$B_{x1}(e^{j\omega})$')
+ax1.plot(w_M,Sx1_per_bias,label = r'$B(e^{j\omega})$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_cor_bias,label = r'$B(e^{j\omega})$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_2_bias,label = r'$B(e^{j\omega})$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_4_bias,label = r'$B(e^{j\omega})$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_16_bias,label = r'$B(e^{j\omega})$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_64_bias,label = r'$B(e^{j\omega})$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_16_bias,label = r'$B(e^{j\omega})$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_64_bias,label = r'$B(e^{j\omega})$ Welch L=64 ($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx1_bias.png')
+# plt.show()
+# plt.close()
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$Var(e^{j\omega})$ for Mc'+f'={Mc}')
+ax1.set_ylabel(r'$V_{x1}(e^{j\omega})$')
+ax1.set_xlabel(r'$\omega$')
+ax1.plot(w_M,Sx1_per_var,label = r'$V(e^{j\omega})$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_cor_var,label = r'$V(e^{j\omega})$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_2_var,label = r'$V(e^{j\omega})$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_4_var,label = r'$V(e^{j\omega})$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_16_var,label = r'$V(e^{j\omega})$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_64_var,label = r'$V(e^{j\omega})$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_16_var,label = r'$V(e^{j\omega})$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_64_var,label = r'$V(e^{j\omega})$ Welch L=64 ($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx1_var.png')
+# plt.show()
+# plt.close()
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$MSE(e^{j\omega})$for Mc'+f'={Mc}')
+ax1.set_ylabel(r'$MSE_{x1}(e^{j\omega})$')
+ax1.set_xlabel(r'$\omega$')
+ax1.plot(w_M,Sx1_per_mse,label = r'$MSE(e^{j\omega})$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_cor_mse,label = r'$MSE(e^{j\omega})$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_2_mse,label = r'$MSE(e^{j\omega})$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bt_4_mse,label = r'$MSE(e^{j\omega})$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_16_mse,label = r'$MSE(e^{j\omega})$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_bartlet_64_mse,label = r'$MSE(e^{j\omega})$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_16_mse,label = r'$MSE(e^{j\omega})$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx1_welch_64_mse,label = r'$MSE(e^{j\omega})$ Welch L=64 ($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx1_mse.png')
+# plt.show()
+
+
+################ x2
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$\bar{S}_{x2}$ Estimation for Mc'+f'={Mc}')
+ax1.set_ylabel(r'$\bar{S}_{x2}(e^{j\omega})$')
+ax1.set_xlabel(r'$\omega$')
+ax1.plot(w_M,Sx2_per_bar,label = r'$\bar{S}_{x2}$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_cor_bar,label = r'$\bar{S}_{x2}$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_2_bar,label = r'$\bar{S}_{x2}$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_4_bar,label = r'$\bar{S}_{x2}$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_16_bar,label = r'$\bar{S}_{x2}$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_64_bar,label = r'$\bar{S}_{x2}$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_16_bar,label = r'$\bar{S}_{x2}$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_64_bar,label = r'$\bar{S}_{x2}$ Welch L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sxx1(w_M),linewidth=1.5,label = r'${S}_{x2}$ True($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx2_bar.png')
+
+# plt.close()
+# plt.show()
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$B(e^{j\omega})$ for Mc'+f'={Mc}')
+ax1.set_ylabel(r'$B_{x2}(e^{j\omega})$')
+ax1.set_xlabel(r'$\omega$')
+ax1.plot(w_M,Sx2_per_bias,label = r'$B(e^{j\omega})$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_cor_bias,label = r'$B(e^{j\omega})$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_2_bias,label = r'$B(e^{j\omega})$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_4_bias,label = r'$B(e^{j\omega})$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_16_bias,label = r'$B(e^{j\omega})$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_64_bias,label = r'$B(e^{j\omega})$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_16_bias,label = r'$B(e^{j\omega})$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_64_bias,label = r'$B(e^{j\omega})$ Welch L=64 ($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx2_bias.png')
+# plt.show()
+# plt.close()
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$Var(e^{j\omega})$ for Mc'+f'={Mc}')
+ax1.set_ylabel(r'$V_{x2}(e^{j\omega})$')
+ax1.set_xlabel(r'$\omega$')
+ax1.plot(w_M,Sx2_per_var,label = r'$V(e^{j\omega})$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_cor_var,label = r'$V(e^{j\omega})$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_2_var,label = r'$V(e^{j\omega})$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_4_var,label = r'$V(e^{j\omega})$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_16_var,label = r'$V(e^{j\omega})$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_64_var,label = r'$V(e^{j\omega})$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_16_var,label = r'$V(e^{j\omega})$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_64_var,label = r'$V(e^{j\omega})$ Welch L=64 ($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx2_var.png')
+# plt.show()
+# plt.close()
+
+fig,(ax1) = plt.subplots(nrows=1,ncols=1,figsize=(10,10))
+fig.suptitle(r'$MSE(e^{j\omega})$for Mc'+f'={Mc}')
+ax1.set_ylabel(r'$MSE_{x2}(e^{j\omega})$')
+ax1.set_xlabel(r'$\omega$')
+ax1.plot(w_M,Sx2_per_mse,label = r'$MSE(e^{j\omega})$ Periodegram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_cor_mse,label = r'$MSE(e^{j\omega})$ Correlogram ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_2_mse,label = r'$MSE(e^{j\omega})$ BT L=2 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bt_4_mse,label = r'$MSE(e^{j\omega})$ BT L=4 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_16_mse,label = r'$MSE(e^{j\omega})$ Bartlet L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_bartlet_64_mse,label = r'$MSE(e^{j\omega})$ Bartlet L=64 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_16_mse,label = r'$MSE(e^{j\omega})$ Welch L=16 ($e^{j\omega} $)')
+ax1.plot(w_M,Sx2_welch_64_mse,label = r'$MSE(e^{j\omega})$ Welch L=64 ($e^{j\omega} $)')
+plt.legend()
+plt.savefig('pics/q3_sx2_mse.png')
+# plt.show()
+
+
+
+
