945274a1401775efbacbd1d32cdd870567e87f02
Chen Sror <csror@maxlinear.com>
2023-06-03 14:26:01 +0300

    add comments

--- code_ex_1/utils.py
+++ code_ex_1/utils.py
@@ -1,20 +1,33 @@
+
+########################################################################
+#                      Ssp 1 - Programing Assigment                   
+#   File Name: utils.py
+#   Content: Helper function and methods fot spectral_estimation.py
+#
+#   Name: Yoav Ellinson                           Name: Chen Yaakov Sror
+#   Id: 206036949                                 Id:   203531645
+########################################################################
+
+########################################################################
+                        # Defines and Includes
+########################################################################
 import numpy as np
 
+########################################################################
+#                     Functions For Question 2
+########################################################################
 
-# stats functions
 def get_Sx_bar(Sx, M=4096):
     Mc = len(Sx)
     Sx = np.array(Sx).reshape(Mc, int(M/2)+1)
     Sx_mean = np.sum(Sx, axis=0)/Mc
     return Sx_mean.flatten()
 
-
 def get_bias(Sx_bar, Sx_true):
     if np.array(Sx_true).shape == np.array(Sx_bar).shape:
         return Sx_bar - Sx_true
     else:
         return None
-
 
 def get_var(Sx, Sx_bar, M=4096):
     Mc = len(Sx)
@@ -24,22 +37,17 @@ def get_var(Sx, Sx_bar, M=4096):
         var += np.abs(Sx[m] - Sx_bar)**2
     return (var/Mc).flatten()
 
-
 def get_MSE(var, bias):
     return var + bias**2
-
 
 def total_bias(B):
     return np.sum(B**2)/len(B)
 
-
 def total_var(var):
     return np.sum(var)/len(var)
 
-
 def total_mse(mse):
     return np.sum(mse)/len(mse)
-
 
 def get_all_stats(Sx, Sx_true, M=4096):
     Sx_bar = get_Sx_bar(Sx)
@@ -51,8 +59,10 @@ def get_all_stats(Sx, Sx_true, M=4096):
     var_total = total_var(var)
     return Sx_bar, B, var, mse, bias_total, var_total, mse_total
 
+########################################################################
+#                        Helper Functions
+########################################################################
 
-# helper functions
 def create_wl_mat(M=4096):
     w_M = np.linspace(0, 2*np.pi, M)[:int(M/2)+1]
     w = np.concatenate((-w_M[::-1], w_M[1:]))
@@ -61,11 +71,12 @@ def create_wl_mat(M=4096):
     l[0, :] = np.arange(-int(M/2), int(M/2)+1, 1)
     return np.dot(w_mat, l)
 
-
 WL = create_wl_mat()  # matrix of ls and exp(-jwl)
 
+########################################################################
+#                        Periodegram Function
+########################################################################
 
-# methods function
 def calc_periodegram(x, M=4096):
     N = len(x)
     X = np.fft.fft(x, n=M)
@@ -73,6 +84,9 @@ def calc_periodegram(x, M=4096):
     w_M = np.linspace(0, 2*np.pi, M)[:int(M/2)+1]
     return Sx_per, w_M
 
+########################################################################
+#                        Correlogram Function
+########################################################################
 
 def calc_correlogram(x, M=4096):
     N = len(x)
@@ -82,6 +96,13 @@ def calc_correlogram(x, M=4096):
     Sx_cor = np.dot(np.exp(-1j*WL), Rx_hat).real[-2049:]
     return Sx_cor, w_M
 
+########################################################################
+#                        Functions For Question 3
+########################################################################
+
+########################################################################
+#                         Blackman-Tukey Function
+########################################################################
 
 def calc_blackman_tukey(x, L=1, M=4096):
     N = len(x)
@@ -96,6 +117,9 @@ def calc_blackman_tukey(x, L=1, M=4096):
     Sx_cor = np.dot(np.exp(-1j*WL), Rx_hat_win).real[-2049:]
     return Sx_cor, w_M
 
+########################################################################
+#                             Bartlet Function
+########################################################################
 
 def calc_bartlet(x, k, M=4096):
     N = len(x)
@@ -112,6 +136,9 @@ def calc_bartlet(x, k, M=4096):
         print('not a valid value for k')
         return None, None
 
+########################################################################
+#                             Welch Function
+########################################################################
 
 def calc_welch(x, k, D, L, M=4096):
     N = len(x)
@@ -123,33 +150,13 @@ def calc_welch(x, k, D, L, M=4096):
     return Sx_b, w_M
 
 
-# def Rxx_hat_b(x,two_sided = False): # creates the auto-corollation series of x, if two sided it creates it for the negative l values as well
-#     sum_n =np.array([np.dot(x[l:l+len(x)-1-abs(l)],x[0:len(x)-1-abs(l)].T) for l in range(len(x))])
-#     if two_sided:
-#         tmp = np.zeros(len(x)*2 -1)
-#         tmp[len(x)-1:] = sum_n
-#         tmp[:len(x)] = sum_n[::-1]
-#         return (1/len(x))*tmp
-#     else:
-#         return (1/len(x))*sum_n
-
-
-# def calc_periodegram_correlogram(x,M=4096): #returns Sx_periodogram,Sx_correlogram, w - all the correct omegas
-#     #old function
-#     #periodogram
-#     x = np.pad(x,(0,int(M/2)+1-x.shape[0]),'constant')
-#     X = np.fft.fft(x,n=M)
-#     Sx_per = (1/M)*(np.abs(X)**2)[:int(M/2)+1]
-#     w_M = np.linspace(0,2*np.pi,M)[:int(M/2)+1]
-#     #correlogram
-#     # Rx_hat =Rxx_hat_b(x,two_sided=True) #with a loop - not accurate enough
-#     Rx_hat = np.correlate(x,x,'full')/M
-#     l0 = np.argmax(Rx_hat)
-#     # Sx_cor = [np.sum([Rx_hat[l]*np.exp(-1j*w*(l0-l)) for l in range(Rx_hat.shape[0])]) for w in w_M] #with a loop
-#     w = np.concatenate((-w_M[::-1],w_M[1:])) #with matrix multipication
-#     w_mat = np.repeat(w.reshape(w.shape[0],1),w.shape[0],axis=1)
-#     l = np.zeros(w_mat.shape)
-#     l[0,:] = np.arange(-int(M/2),int(M/2)+1,1)
-#     wl = np.dot(w_mat,l)
-#     Sx_cor = np.dot(np.exp(-1j*wl),Rx_hat).real[-2049:]
-#     return Sx_per,Sx_cor,w_M
+#    _________  ________  ___  ___       ___          ________             ________  ___  ___       ___     
+#   |\___   ___\\_____  \|\  \|\  \     |\  \        |\   __  \           |\   ____\|\  \|\  \     |\  \    
+#   \|___ \  \_|\|___/  /\ \  \ \  \    \ \  \       \ \  \|\  \  /\      \ \  \___|\ \  \ \  \    \ \  \   
+#        \ \  \     /  / /\ \  \ \  \    \ \  \       \ \__     \/  \      \ \  \  __\ \  \ \  \    \ \  \  
+#         \ \  \   /  /_/__\ \  \ \  \____\ \  \       \|_/  __     /|      \ \  \|\  \ \  \ \  \____\ \  \ 
+#          \ \__\ |\________\ \__\ \_______\ \__\        /  /_|\   / /       \ \_______\ \__\ \_______\ \__\
+#           \|__|  \|_______|\|__|\|_______|\|__|       /_______   \/         \|_______|\|__|\|_______|\|__|
+#                                                       |_______|\__\                                       
+#                                                               \|__|                                       
+                                                                                                
